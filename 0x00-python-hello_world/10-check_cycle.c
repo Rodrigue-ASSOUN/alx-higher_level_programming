8a8e4878df45912e89f41b46b98dbcb21d6b1698
Nahima-ayni <nahimamadey1@gmail.com>
2023-11-27 09:43:16 -0800

    technical hare

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * check_cycle - function name
+ * list: listint
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
+}
