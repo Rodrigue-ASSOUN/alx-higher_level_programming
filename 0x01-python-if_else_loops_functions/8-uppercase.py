c27c3ed4adda5aaf9c7ed442e20dfc6a05540498
Nahima-ayni <nahimamadey1@gmail.com>
2023-11-28 20:56:15 +0300

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in s:
+        print("{:c}".format(ord(char) - 32) if 'a' <= char <= 'z'
+                                                            else char, end="")
+    print()
