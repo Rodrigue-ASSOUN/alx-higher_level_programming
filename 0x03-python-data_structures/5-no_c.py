970669b88354684ab9b534008fbeb187c27d803b
Nahima-ayni <nahimamadey1@gmail.com>
2023-12-02 18:18:54 +0300

    removes all character

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for i in my_string:
-        if i.lower() not in {'c', 'C':
-            new_string += i
-
+    for char in my_string:
+        if char.lower() not in {'c', 'C'}:
+            new_string += char
     return new_string
