471052428db08363ef86749f8f4c7a95735eb3b0
Nahima-ayni <nahimamadey1@gmail.com>
2023-12-18 17:23:47 +0300

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,8 @@ def list_division(my_list_1, my_list_2, list_length):
     lists = [0] * list_length
     for i in range(list_length):
         try:
-            if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+            if not isinstance(my_list_1[i], (int, float))
+            or not isinstance(my_list_2[i], (int, float)):
                 print("wrong type")
                 lists[i] = 0
             else:
