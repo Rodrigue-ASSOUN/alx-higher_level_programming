b0b33a2e3a114e00e06593c64d4a285571c00fa5
Nahima-ayni <nahimamadey1@gmail.com>
2024-01-04 22:15:23 +0300

    6. How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+Rectangle.number_of_instances += 1
     
     @property
     def width(self):
@@ -66,4 +66,4 @@ class Rectangle:
     def __del__(self):
         """destructor"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+Rectangle.number_of_instances -= 1
