b6f3f4a3e376e272b918d9f5b4fb50e8e25b7eb7
Nahima-ayni <nahimamadey1@gmail.com>
2024-01-08 14:56:19 +0300

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to inherit from a list"""
+
+
+class MyList:
+    """class to list in ascending order"""
+
+    def print_sorted(self):
+        """print in ascending order"""
+        print(sorted(self))
