021b7a1a4023fb47711c7c060b904d26513c287c
Nahima-ayni <nahimamadey1@gmail.com>
2024-02-13 06:26:11 -0800

    11. Select the best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
--- lists all records of the table, score in ascending order
+-- lists all records of the table
+-- score in ascending order
+-- The database name will be passed as an argument of the mysql command
 
 SELECT score, name FROM second_table ORDER BY score DESC;
