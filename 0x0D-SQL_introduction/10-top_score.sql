e006bf7b625e9151667d4d2d67ec4dcc3d42e52f
Nahima-ayni <nahimamadey1@gmail.com>
2024-02-13 06:29:53 -0800

    11. Select the best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
+-- script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- lists all records of the table
 -- score in ascending order
 -- The database name will be passed as an argument of the mysql command
