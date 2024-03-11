43d7a6c56709e96e57078d47d417c91a3443a61f
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-11 12:15:43 +0300

    concanate

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if  (args.length === 4) {
-    const result = args[2] + ' is ' + args[3];
-    console.log(result);
-}
+console.log(args[2] + ' is ' + args[3]);
