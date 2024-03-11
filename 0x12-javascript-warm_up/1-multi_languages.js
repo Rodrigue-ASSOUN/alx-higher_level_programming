106fd9843cfe2b1c85f39ad658b1b1520ddf8aed
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-11 10:23:18 +0300

    script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const lines = 'C is fun';
+const lineTwo = 'Python is cool';
+const lineThree = 'JavaScript is amazing';
+
+console.log(lines);
+console.log(lineTwo);
+console.log(lineThree);
