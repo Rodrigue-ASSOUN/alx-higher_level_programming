72088f3666fc272b8050fa2f2d21638609beda14
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-12 15:37:06 +0300

    advanced

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,4 +8,3 @@ const content2 = fs.readFileSync(sourceFilePath2, 'utf8');
 const concatenatedContent = `${content1.trim()}\n${content2.trim()}`;
 
 fs.writeFileSync(destinationFilePath, concatenatedContent);
-
