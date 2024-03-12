eeaae3ef22da25c4f5edbfef6d4f0e57c442b9bb
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-12 14:36:09 +0300

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-exports.converter = function (number, base) {
-  return number.toString(base);
+exports.converter = function (base) {
+  return function convertToBase (n) {
+    if (n < base) {
+      return n.toString(base);
+    } else {
+      return convertToBase(Math.floor(n / base)) + (n % base).toString(base);
+    }
+  };
 };
