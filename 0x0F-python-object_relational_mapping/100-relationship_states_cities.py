62ae74a771f22bbf14d004f7fe84b1f379c38d8b
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-17 10:13:11 +0300

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,20 +14,16 @@ from relationship_city import City
 if __name__ == "__main__":
     """Create State California with City San Francisco"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
+                           .format(username, password, database))
 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    california = State(name='California')
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
 
-    san_francisco = City(name='San Francisco')
-
-    california.cities.append(san_francisco)
-
-    session.add(california)
-
+    session.add(new_state)
     session.commit()
-
+    session.close()
