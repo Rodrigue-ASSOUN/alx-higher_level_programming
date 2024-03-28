fc799c2a5e29af5b87ff287a7ef4f00327c28ab6
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-28 13:05:26 +0300

    network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,13 @@ import requests
 if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-    
+
     url = "https://api.github.com/repos/{}/{}/commits".format(
         owner_name, repository_name)
 
     response = requests.get(url, auth=(owner_name, repository_name))
     commit_list = response.json()
-    
+
     try:
         for i in range(10):
             print("{}: {}".format(
