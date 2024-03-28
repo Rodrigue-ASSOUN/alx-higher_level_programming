421682db6727325fa442c7bf87c1349930eee3e3
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-28 12:57:56 +0300

    network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,17 +6,9 @@ import requests
 if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-
-    url = "https://api.github.com/repos/{}/{}commits".format(
-            owner_name, repository_name)
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()
-
-        for commit in commits[:10]:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print(f"Error: {response.status_code}")
+    response = requests.get(
+        'https://developer.github.com/v3/repos/commits/',
+        auth=(
+            repository_name,
+            owner_name))
+    print(response.json().get("id, name"))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     """
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    print("Body response: ")
-    print("\t- type: ", type(response.text))
+    print("Body response:")
+    print("\t- type:", type(response.text))
     print("\t- content:", response.text)
