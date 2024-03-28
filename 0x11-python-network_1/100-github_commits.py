ccf4ef1f9877b3891a79d7493b5ff77583b5a1b4
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-28 12:28:16 +0300

    network #1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""10. Time for an interview!"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    response = requests.get('https://developer.github.com/v3/repos/commits/', auth=(
+        repository_name, owner_name))
+    print(response.json().get("id, name"))
