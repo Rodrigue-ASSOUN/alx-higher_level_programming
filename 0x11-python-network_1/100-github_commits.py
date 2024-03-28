abd8131eda1d90ff758bac68c3a68f290699d411
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-28 12:34:25 +0300

    network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,6 +6,15 @@ import requests
 if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-    response = requests.get('https://developer.github.com/v3/repos/commits/', auth=(
-        repository_name, owner_name))
-    print(response.json().get("id, name"))
+    
+    response = requests.get(
+            'https://developer.github.com/v3/repos/commits/', auth=(repository_name, owner_name))
+    commits = response.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
