9fa9d66106edc0837601be88daf08cc57b966774
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-23 20:51:38 +0300

    0-readme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// The first argument is the file path
+const filePath = process.argv[2];
+
+// The second argument is the string to write
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', function (err) {
+  if (err) {
+    // If an error occurred during writing, print the error object
+    console.error(err);
+  }
+});
