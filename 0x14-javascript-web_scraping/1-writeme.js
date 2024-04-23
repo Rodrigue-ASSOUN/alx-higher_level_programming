dad355fb326f7d20aef457fd80c9bdcec09f17b0
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-23 20:52:02 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // The first argument is the file path
 const filePath = process.argv[2];
 
-// The second argument is the string to write
+//second argument is the string to write
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf8', function (err) {
