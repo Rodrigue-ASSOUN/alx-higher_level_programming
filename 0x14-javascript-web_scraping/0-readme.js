763a8faebb84fdb707731e3bc1681ac2fc398c7f
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-23 20:58:42 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,15 @@
+#!/usr/bin/node
+
 const fs = require('fs');
 
-// The first argument is the file path
+// Path to the file we want to read
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', function (err, data) {
+// Asynchronously read from the file
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    // If an error occurred during the reading, print the error object
     console.error(err);
-  } else {
-    // The content of the file must be read in utf-8
-    console.log(data);
+    return;
   }
+  console.log(data);
 });
