90cbf80c77dce850a345b0fbb1cd6cd0bb2afae9
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-23 22:35:23 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,26 @@
-const request = require('request');
-const movieId = 3;
+#!/usr/bin/node
 
+const request = require('request');
+
+const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    movie.characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
     });
   }
 });
