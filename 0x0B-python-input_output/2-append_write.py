cdc75114a709940deca611d8dd87848fee1a012c
Rodrigue ASSOUN <assounrodrigue5@gmail.com>
2024-12-09 15:19:09 +0000

    Add of hello world in output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """ function that appends a string at the end of a text file (UTF8) and
-        returns the number of characters added"""
+        returns the*number of characters added"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
